729eb7b6fcba88a3711a5c1698966da3defd64d0
Abdallahkajj <busy.heart@live.com>
2021-03-01 14:36:21 +0300

    Add project files.

---
+++ EFCore/Models/EFContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore.Models
+{
+    internal class EFContext : DbContext
+    {
+        private const string connectionString = @"Server = DESKTOP-6E0MHSD\APO; Database= EFCore; Trusted_Connection=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
